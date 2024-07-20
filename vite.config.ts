61e329ea4ac49cc189b017fa8b7f47461ec53cbf
luisalbertoj <luisalbertoj.tober@gmail.com>
2024-07-20 16:29:07 -0500

    send mail

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,11 @@ export default defineConfig(({ mode }) => ({
           '/api/v1/**': {
             cors: true,
             headers: {
-              'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+              'Access-Control-Allow-Methods':
+                'GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS',
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': 'true',
+              'Access-Control-Allow-Headers': '*',
             },
           },
         },
