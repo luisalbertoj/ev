19f2bb908964e333a7a2ee9fc154f80ceeeaf52d
luisalbertoj <luisalbertoj.tober@gmail.com>
2024-07-22 17:20:05 -0500

    cors configure

--- src/server/middleware/cors.ts
+++ src/server/middleware/cors.ts
@@ -11,6 +11,32 @@ export default defineEventHandler((event: H3Event) => {
   // Obtén el origen de la solicitud
   const origin = event.node.req.headers.origin || '';
 
+  // Manejar solicitudes OPTIONS
+  if (event.method === 'OPTIONS' || !event.method) {
+    if (allowedOrigins.includes(origin)) {
+      setResponseHeaders(event, {
+        'Access-Control-Allow-Methods':
+          'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Max-Age': '86400', // Cachear la respuesta por un día (opcional)
+      });
+    } else {
+      setResponseHeaders(event, {
+        'Access-Control-Allow-Methods':
+          'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Max-Age': '86400', // Cachear la respuesta por un día (opcional)
+      });
+    }
+    event.node.res.statusCode = 200;
+    event.node.res.statusMessage = 'OK';
+    return 'OK';
+  }
+
   // Verifica si el origen está en la lista de dominios permitidos
   if (allowedOrigins.includes(origin)) {
     setResponseHeaders(event, {
@@ -19,19 +45,5 @@ export default defineEventHandler((event: H3Event) => {
       'Access-Control-Allow-Credentials': 'true',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     });
-  }
-
-  // Manejar solicitudes OPTIONS
-  if (event.method === 'OPTIONS' || !event.method) {
-    setResponseHeaders(event, {
-      'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      'Access-Control-Allow-Origin': origin || '*',
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      'Access-Control-Max-Age': '86400', // Cachear la respuesta por un día (opcional)
-    });
-    event.node.res.statusCode = 200;
-    event.node.res.statusMessage = 'OK';
-    return 'OK';
   }
 });
